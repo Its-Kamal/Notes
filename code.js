d02a16506e543c96679fd8097a7ee4109b435ae2
Kamal Singh <107418524+Its-Kamal@users.noreply.github.com>
2022-12-13 23:24:08 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,16 @@
 const NewNote = document.querySelector("#header #add-note");
 const MainArea = document.querySelector("#main");
-var NoteHTML = `<i class="fa-solid fa-expand" id="expand-note"></i><i class="fa-solid fa-trash" id="delete-note"></i><input type="text" id="note-title" placeholder="Title here"><textarea id="note-content" placeholder="Add Notes here" cols="30" rows="10"></textarea>`;
+var NoteHTML = `<i class="fa-regular fa-trash-can"></i><input type="text" id="note-title" placeholder="Title here"><textarea id="note-content" placeholder="Add Notes here"></textarea>`;
 function localupdate(){
     var NoteDataTitle= MainArea.querySelectorAll("#note-title");
     var NoteDataContent= MainArea.querySelectorAll("#note-content");
     var TitleDataAR=[];
     var ContentDataAR=[];
-    console.log(NoteDataTitle);
     NoteDataTitle.forEach(function (item) {
         TitleDataAR.push(item.value);
-        // console.log(TitleDataAR);
     })
     NoteDataContent.forEach(function (item) {
         ContentDataAR.push(item.value);
-        // console.log(ContentDataAR);
     })
     localStorage.setItem("Notes_Title",JSON.stringify(TitleDataAR));
     localStorage.setItem("Notes_Content",JSON.stringify(ContentDataAR));
@@ -24,21 +21,19 @@ const AddingNote=() => {
     note.classList = "note";
     MainArea.appendChild(note);
     note.insertAdjacentHTML("afterbegin", NoteHTML);
-    note.querySelector("#expand-note").addEventListener("click", function(){
-      if (note.style.height == "6vh") {
-        note.style.height = "30vh";
-      } else {
-        note.style.height = "6vh";
-      }
-    });
-    note.querySelector("#delete-note").addEventListener("click", function(){
+    function NoteExpand(){note.style.height = "30vh";}
+    function NoteContract(){note.style.height = "6vh";}
+    note.querySelector(".note #note-title").addEventListener("focus",NoteExpand)
+    note.querySelector(".note #note-title").addEventListener("focusout",NoteContract)
+    note.querySelector(".note #note-content").addEventListener("focus",NoteExpand)
+    note.querySelector(".note #note-content").addEventListener("focusout",NoteContract)
+    note.querySelector(".fa-trash-can").addEventListener("click", function(){
       note.remove();
       localupdate();
     });
     const NoteTitle=document.querySelector('#note-title');
     const NoteContent=document.querySelector('#note-content');
-    NoteTitle.addEventListener('input',localupdate());
-    NoteContent.addEventListener('input',localupdate());
+    setInterval(() => {localupdate();}, 100);
 }
 function GetLocalData() {
     var GetTitle=[];
@@ -50,13 +45,13 @@ function GetLocalData() {
         GetContent.push(name);
     })
     for (let index = 0; index < GetTitle.length; index++) {
-        NoteHTML = `<i class="fa-solid fa-expand" id="expand-note"></i><i class="fa-solid fa-trash" id="delete-note"></i><input type="text" id="note-title" placeholder="Title here" value='${GetTitle[index]}'><textarea id="note-content" placeholder="Add Notes here" cols="30" rows="10">${GetContent[index]}</textarea>`;
+        NoteHTML = `<i class="fa-regular fa-trash-can"></i><input type="text" id="note-title" placeholder="Title here" value='${GetTitle[index]}'><textarea id="note-content" placeholder="Add Notes here">${GetContent[index]}</textarea>`;
         AddingNote();
+        console.log(index+1 +" "+"Notes Fetched");
     }
 }
 if (localStorage.length>0) {
     GetLocalData();
 }
-NoteHTML = `<i class="fa-solid fa-expand" id="expand-note"></i><i class="fa-solid fa-trash" id="delete-note"></i><input type="text" id="note-title" placeholder="Title here"><textarea id="note-content" placeholder="Add Notes here" cols="30" rows="10"></textarea>`;
+NoteHTML = `<i class="fa-regular fa-trash-can"></i><input type="text" id="note-title" placeholder="Title here"><textarea id="note-content" placeholder="Add Notes here"></textarea>`;
 NewNote.addEventListener("click", AddingNote);
-
